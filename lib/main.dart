6f658e4102e7897fd97b85b2fdb69aa3706eabf3
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-12-01 08:08:39 +0900

    ルーター分離。firebase-config分離

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'detail_screen.dart';
 import 'error.dart';
-import 'home_screen.dart';
 import 'loading.dart';
-import 'public_hook_view.dart';
-import 'public_screen.dart';
-import 'stream_public_screen.dart';
-import 'unknown_screen.dart';
+import 'navigation.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -43,44 +38,6 @@ class App extends StatelessWidget {
 
         // Otherwise, show something whilst waiting for initialization to complete
         return MaterialApp(title: 'Welcome to Flutter', home: Loading());
-      },
-    );
-  }
-}
-
-class Navigation extends StatelessWidget {
-  final String _title;
-  Navigation(this._title, {Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      onGenerateRoute: (settings) {
-        // Handle '/'
-        if (settings.name == '/') {
-          return MaterialPageRoute(builder: (context) => HomeScreen());
-        }
-
-        // Handle '/details/:id'
-        var uri = Uri.parse(settings.name);
-        if (uri.pathSegments.length == 2 &&
-            uri.pathSegments.first == 'details') {
-          var id = uri.pathSegments[1];
-          return MaterialPageRoute(builder: (context) => DetailScreen(id: id));
-        }
-
-        if (uri.pathSegments.first == 'public') {
-          return MaterialPageRoute(builder: (context) => PublicScreen());
-        }
-        if (uri.pathSegments.first == 'public_stream') {
-          return MaterialPageRoute(builder: (context) => StreamPublicScreen());
-        }
-        if (uri.pathSegments.first == 'public_hook') {
-          return MaterialPageRoute(builder: (context) => PublicView());
-        }
-
-        return MaterialPageRoute(builder: (context) => UnknownScreen());
       },
     );
   }
---
+++ lib/navigation.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'detail_screen.dart';
+import 'home_screen.dart';
+import 'public_hook_view.dart';
+import 'public_screen.dart';
+import 'stream_public_screen.dart';
+import 'unknown_screen.dart';
+
+class Navigation extends StatelessWidget {
+  final String _title;
+  Navigation(this._title, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      onGenerateRoute: (settings) {
+        // Handle '/'
+        if (settings.name == '/') {
+          return MaterialPageRoute(builder: (context) => HomeScreen());
+        }
+
+        // Handle '/details/:id'
+        var uri = Uri.parse(settings.name);
+        if (uri.pathSegments.length == 2 &&
+            uri.pathSegments.first == 'details') {
+          var id = uri.pathSegments[1];
+          return MaterialPageRoute(builder: (context) => DetailScreen(id: id));
+        }
+
+        if (uri.pathSegments.first == 'public') {
+          return MaterialPageRoute(builder: (context) => PublicScreen());
+        }
+        if (uri.pathSegments.first == 'public_stream') {
+          return MaterialPageRoute(builder: (context) => StreamPublicScreen());
+        }
+        if (uri.pathSegments.first == 'public_hook') {
+          return MaterialPageRoute(builder: (context) => PublicView());
+        }
+
+        return MaterialPageRoute(builder: (context) => UnknownScreen());
+      },
+    );
+  }
+}
