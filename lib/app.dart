a869eecf3f898af3eb9c36dcab646494f77e756a
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-12-04 16:55:41 +0900

    main: await Firebase.initializeApp(); のパターンだと全部動く

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-import 'error.dart';
-import 'loading.dart';
 import 'navigation.dart';
 
 class App extends HookWidget {
@@ -11,14 +8,6 @@ class App extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final snapshot = useFuture(Firebase.initializeApp());
-    if (snapshot.hasError) {
-      return MaterialApp(title: 'Welcome to Flutter', home: Error());
-    }
-    if (snapshot.hasData) {
-      return Navigation('Welcome to Flutter');
-    }
-    // Otherwise, show something whilst waiting for initialization to complete
-    return MaterialApp(title: 'Welcome to Flutter', home: Loading());
+    return Navigation('Welcome to Flutter');
   }
 }
