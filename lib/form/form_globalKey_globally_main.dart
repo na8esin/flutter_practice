2b540855f353f4f75ae2437992130a85b1d5fab2
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-12-15 17:57:12 +0900

    GlobalKeyをgloballyに使うサンプル

---
+++ lib/form/form_globalKey_globally_main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+/*
+ GlobalKeyをgloballyに使うサンプル
+*/
+
+final formKey = GlobalKey<FormState>();
+void main() => runApp(
+      const ProviderScope(child: MyApp()),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Form Validation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MyCustomFormState(),
+      ),
+    );
+  }
+}
+
+class MyFormController extends StateNotifier<String> {
+  MyFormController() : super('');
+  bool validate() {
+    if (state.isNotEmpty) {
+      return true;
+    }
+    return false;
+  }
+
+  String validator(String value) {
+    print('validator');
+    setFormValue(value);
+    if (value.isEmpty) {
+      return 'Please enter some text';
+    }
+    return null;
+  }
+
+  void setFormValue(String value) {
+    state = value;
+  }
+}
+
+final myFormControllerProvider =
+    StateNotifierProvider((ref) => MyFormController());
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final MyFormController myForm = useProvider(myFormControllerProvider);
+
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: myForm.validator,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
