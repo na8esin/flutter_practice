257236921b90c139b41d91073251091eb4224194
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-12-15 14:36:55 +0900

    hookを使った簡単なカウンターサンプル

---
+++ lib/sample/simple_hook_main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MaterialApp(home: MyHomePage())));
+}
+
+class CounterController extends StateNotifier<int> {
+  CounterController() : super(0);
+  void increment() => state++;
+}
+
+/// Providers are declared globally and specifies how to create a state
+final counterControllerProvider =
+    StateNotifierProvider((ref) => CounterController());
+
+class MyHomePage extends HookWidget {
+  const MyHomePage({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final counterState = useProvider(counterControllerProvider.state);
+    final counter = useProvider(counterControllerProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You have pushed the button this many times'),
+            Text('${counterState}',
+                style: Theme.of(context).textTheme.headline4),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
