fc276409ea6d5fe968d1d3e63deae21de705620a
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-12-15 09:36:37 +0900

    Scaffoldを横に２つならべてみた

---
+++ lib/sample/simple_routes_main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    routes: <String, WidgetBuilder>{
+      '/': (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Home Route'),
+          ),
+        );
+      },
+      '/about': (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('About Route'),
+          ),
+        );
+      },
+      '/no_sca': (BuildContext context) {
+        return Text('hello');
+      },
+    },
+  ));
+}
