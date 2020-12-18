85ee6296096bff17ecd23cd43afe21a24a4ef403
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-12-18 16:13:40 +0900

    OpenContainerとNavigatorのサンプル

---
+++ lib/OpenContainer/OpenContainer_Navigator_main.dart
@@ -0,0 +1,82 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: App(),
+  ));
+}
+
+// Navigatorが間にあっても遷移はする
+class MyNavigator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(onGenerateRoute: (settings) {
+      switch (settings.name) {
+        case '/':
+          return MaterialPageRoute<void>(
+            builder: (context) => App(),
+          );
+          break;
+        default:
+          return MaterialPageRoute<void>(
+            builder: (context) => Scaffold(
+              body: Text('error'),
+            ),
+          );
+      }
+    });
+  }
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OpenContainer<bool>(
+      transitionType: ContainerTransitionType.fade,
+      openBuilder: (BuildContext _, VoidCallback openContainer) {
+        return const _DetailsPage();
+      },
+      onClosed: (bool isMarkedAsDone) {
+        if (isMarkedAsDone ?? false)
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Marked as done!'),
+          ));
+      },
+      tappable: false,
+      closedShape: const RoundedRectangleBorder(),
+      closedElevation: 0.0,
+      closedBuilder: (BuildContext _, VoidCallback openContainer) {
+        return ListTile(
+          leading: Image.asset(
+            'assets/avatar_logo.png',
+            width: 40,
+          ),
+          onTap: openContainer,
+          title: Text('List item 1'),
+          subtitle: const Text('Secondary text'),
+        );
+      },
+    );
+  }
+}
+
+class _DetailsPage extends StatelessWidget {
+  const _DetailsPage();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Details page'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.done),
+            onPressed: () => Navigator.pop(context, true),
+            tooltip: 'Mark as done',
+          )
+        ],
+      ),
+      body: Text('detail'),
+    );
+  }
+}
