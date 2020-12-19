de5b024165cf0b9a479093a591c32185b7771278
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-12-19 11:53:51 +0900

    ProviderListenerを使って、カウントが5になったらshowDialog

---
+++ lib/hook_listener/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MaterialApp(home: MyApp())));
+}
+
+final countStateControllerProvider = StateNotifierProvider(
+  (ref) => CountStateController(
+    CountState(0),
+  ),
+);
+
+class CountState {
+  CountState(this.count);
+  final int count;
+}
+
+class CountStateController extends StateNotifier<CountState> {
+  CountStateController(CountState state) : super(state);
+
+  void increment() {
+    state = CountState(state.count + 1);
+  }
+
+  void erase() {
+    state = CountState(0);
+  }
+}
+
+final countStateProvider = Provider.autoDispose(
+    (ref) => ref.watch(countStateControllerProvider.state));
+
+class MyApp extends HookWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    final prop = useProvider(countStateControllerProvider);
+    return ProviderListener(
+      provider: countStateProvider,
+      onChange: (context, CountState countState) {
+        if (countState.count == 5) {
+          showDialog(
+            context: context,
+            builder: (context) => SimpleDialog(
+              title: Text('New Category'),
+              children: <Widget>[MyForm()],
+            ),
+          );
+        }
+      },
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          // The read method is an utility to read a provider without listening to it
+          onPressed: () => prop.increment(),
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+class MyForm extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Name',
+              ),
+              onChanged: (newValue) {},
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a name';
+                }
+                return null;
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: RaisedButton(
+                  child: Text('Cancel'),
+                  onPressed: () {},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: RaisedButton(
+                  child: Text('OK'),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
