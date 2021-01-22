f77ae9e0adeaaeb94d6236a5eeaaba209844b391
na8esin <na8esin.mpst@gmail.com>
2021-01-22 17:27:14 +0900

    #8 StreamFamily: StreamProviderを二つ使っても大丈夫

--- lib/firestore_ref/StreamFamily.dart
+++ lib/firestore_ref/StreamFamily.dart
@@ -4,10 +4,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// そもそもstream.familyって動くの？
+// - そもそもstream.familyって動くの？ OK
+// - loop とuseProviderが一緒につかない そんなことない
+// - asyncValueのネストがだめ？
 
 final $family = StreamProvider.family;
-
 final publicProvider = $family<String, String>((ref, String id) {
   return FirebaseFirestore.instance
       .collection('publics')
@@ -16,10 +17,21 @@ final publicProvider = $family<String, String>((ref, String id) {
       .map((e) => e.data()['name']);
 });
 
+final publicIdsProvider = StreamProvider<List<String>>((ref) {
+  return FirebaseFirestore.instance
+      .collection('publics')
+      .snapshots()
+      .map((e) => e.docs.map((e) => e.id).toList());
+});
+
 class StreamFamily extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final ids = ['0YPJmTVo631kHkpQgP47', 'FJeLdG1oSla7JGBaguka'];
+    //final ids = ['0YPJmTVo631kHkpQgP47', 'FJeLdG1oSla7JGBaguka'];
+    final List<String> ids = useProvider(publicIdsProvider).when(
+        data: (data) => data,
+        loading: () => ['loading'],
+        error: (e, s) => [e.toString()]);
     return ListView(
       children: ids.map((e) => StreamHookWidget(e)).toList(),
     );
