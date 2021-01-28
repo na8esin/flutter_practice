8fa08039c84bf4d5a5ecd3b427535fccba9c0f99
na8esin <na8esin.mpst@gmail.com>
2021-01-28 17:51:21 +0900

    #16 migrating-from-flutter_driver

--- integration_test/sample_test.dart
+++ integration_test/sample_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+// test_driverなくても動くけど。。。
+// https://github.com/flutter/flutter/issues/72063#issuecomment-742528902
+
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
