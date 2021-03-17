95de773a645491265b46b913ad6b55f5fbfa63ce
na8esin <na8esin.mpst@gmail.com>
2021-03-17 13:26:00 +0900

    rename: firestore_ref -> firestore_sample

---
+++ lib/firestore_sample/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'NoHookNestStreamBuilder.dart';
+import 'NestStreamBuilderRoles.dart';
+import 'NestStreamBuilderRolesHook.dart';
+import 'OnePublicRoles.dart';
+import 'CollectionGroupParent.dart';
+import 'HookPubDetail.dart';
+import 'HookPubDetailRef.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const ProviderScope(child: MaterialApp(home: MyApp())));
+}
+
+List<Map<String, dynamic>> listListTile = [
+  titleBuilder(NoHookNestStreamBuilder()),
+  titleBuilder(NestStreamBuilderRoles()),
+  titleBuilder(NestStreamBuilderRolesHook()),
+  titleBuilder(OnePublicRoles()),
+  titleBuilder(CollectionGroupParent()),
+  titleBuilder(HookPubDetail()),
+  titleBuilder(HookPubDetailRef()),
+];
+
+titleBuilder(Widget clazz) {
+  return {'title': Text(clazz.toStringShort()), 'builder': clazz};
+}
+
+class MyApp extends HookWidget {
+  const MyApp();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView.separated(
+            itemCount: listListTile.length,
+            separatorBuilder: (context, index) => const SizedBox(height: 3),
+            itemBuilder: (context, index) {
+              return ListTile(
+                  title: listListTile[index]['title'],
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Scaffold(
+                                body: listListTile[index]['builder'],
+                              ))));
+            }));
+  }
+}
---
+++ lib/firestore_sample/model/public.dart
@@ -0,0 +1,70 @@
+import 'package:firestore_ref/firestore_ref.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'public.freezed.dart';
+part 'public.g.dart';
+
+@freezed
+abstract class Public with _$Public {
+  // TODO: ここにDtailsを入れたくなるが。。。
+  factory Public({
+    required String name,
+    String subname,
+    @TimestampConverter() DateTime createdAt,
+    @TimestampConverter() DateTime updatedAt,
+  }) = _Public;
+  factory Public.fromJson(Map<String, dynamic> json) => _$PublicFromJson(json);
+}
+
+class PublicField {
+  static const name = 'name';
+  static const subname = 'subname';
+  static const createdAt = 'createdAt';
+  static const updatedAt = 'updatedAt';
+}
+
+final publicsRef = PublicsRef();
+
+class PublicsRef extends CollectionRef<Public, PublicDoc, PublicRef> {
+  PublicsRef() : super(FirebaseFirestore.instance.collection('publics'));
+
+  @override
+  Map<String, dynamic> encode(Public data) =>
+      replacingTimestamp(json: data.toJson());
+
+  @override
+  PublicDoc decode(DocumentSnapshot snapshot, PublicRef docRef) {
+    assert(docRef != null);
+    return PublicDoc(
+      docRef,
+      Public.fromJson(snapshot.data()!),
+    );
+  }
+
+  @override
+  PublicRef docRef(DocumentReference ref) => PublicRef(
+        ref: ref,
+        publicsRef: this,
+      );
+}
+
+class PublicRef extends DocumentRef<Public, PublicDoc> {
+  const PublicRef({
+    required DocumentReference ref,
+    required this.publicsRef,
+  }) : super(
+          ref: ref,
+          collectionRef: publicsRef,
+        );
+
+  final PublicsRef publicsRef;
+}
+
+class PublicDoc extends Document<Public> {
+  const PublicDoc(
+    this.publicRef,
+    Public entity,
+  ) : super(publicRef, entity);
+
+  final PublicRef publicRef;
+}
