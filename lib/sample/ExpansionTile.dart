842dc3f2de905f0af653e98122e5946210cb4709
na8esin <na8esin.mpst@gmail.com>
2021-03-17 15:21:11 +0900

    左メニュ再考: ExpansionTile FloatingActionButtonを押すとExpandedのflexの値が変わる

--- lib/sample/ExpansionTile.dart
+++ lib/sample/ExpansionTile.dart
@@ -3,23 +3,71 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+/// たりない要素としては、ハンバーガー
+
+final flexProvider = StateNotifierProvider((ref) => FlexController());
+
+class FlexController extends StateNotifier<int> {
+  FlexController() : super(2);
+
+  void change() {
+    state = state == 2 ? 1 : 2;
+  }
+}
+
 void main() {
   runApp(ProviderScope(
-      child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Scaffold(
-            body: MyHomePage(),
-          ))));
+      child:
+          MaterialApp(debugShowCheckedModeBanner: false, home: MyScaffold())));
+}
+
+class MyScaffold extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final flex = useProvider(flexProvider);
+    return Scaffold(
+      body: MyHomePage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => flex.change(),
+      ),
+    );
+  }
 }
 
 class MyHomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      title: Text('sample title'),
+    final flex = useProvider(flexProvider.state);
+    return Row(
       children: [
-        Text('Text1'),
-        Text('Text2'),
+        Expanded(
+          flex: flex,
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: ExpansionTile(
+              expandedAlignment: Alignment.centerRight,
+              title: Text('sample title'),
+              children: [
+                // childrenPaddingを使っても子要素同士の空白がうまくあかない
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Text1'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Text2'),
+                ),
+              ],
+            ),
+          ),
+        ),
+        VerticalDivider(thickness: 1, width: 1),
+        Expanded(
+          flex: 8,
+          child: Center(
+            child: Text('Main'),
+          ),
+        )
       ],
     );
   }
